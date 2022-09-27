5f7e306030f7f1d267ff6a9d4d63d8cd2bc0c790
Dipteshpatra <113527751+Dipteshpatra@users.noreply.github.com>
2022-09-27 16:50:09 +0530

    Add files via upload

---
+++ Task/to check a char/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace to_check_a_char
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String str = "abcdefgh";
+            Char value = 'c';
+            Boolean result = str.Contains(value);
+            Console.WriteLine("is c char avilable in the string-"+ result);
+        }
+    }
+}
